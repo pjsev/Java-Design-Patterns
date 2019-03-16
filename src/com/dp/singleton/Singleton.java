918ea75a01b24ceae8d49041c9c713689c79dcbf
Asus <Asus@LAPTOP-FBGTTFA5>
2019-03-16 11:54:46 +0800

    Finalize singleton Demo

--- src/com/dp/singleton/Singleton.java
+++ src/com/dp/singleton/Singleton.java
@@ -1,5 +1,18 @@
 package com.dp.singleton;
 
 public class Singleton {
-
+	
+	private static Singleton singleton = null;
+	public String s;
+	
+	private Singleton() {
+		s = "This is a singleton class";
+	}
+	
+	public static Singleton getInstance() {
+		if (singleton == null) {
+			singleton = new Singleton();
+		}
+		return singleton;
+	}
 }
--- src/com/dp/singleton/SingletonDemo.java
+++ src/com/dp/singleton/SingletonDemo.java
@@ -1,5 +1,10 @@
 package com.dp.singleton;
 
 public class SingletonDemo {
-
+	
+	public static void main(String[] args) {
+		Singleton x = Singleton.getInstance();
+		
+		System.out.println(x.s);
+	}
 }
